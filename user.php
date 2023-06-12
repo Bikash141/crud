4bf54650984957ac098c6918950d3e88f0a8f4c4
Bikash Prasad <bikashprasad1412@gmail.com>
2023-06-12 14:45:03 +0530

    first commit

---
+++ user.php
@@ -0,0 +1,73 @@
+<?php
+include 'connect.php';
+
+if( isset($_POST['submit'] )){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $phone = $_POST['phone'];
+    $query = "insert into `details`(name,email,password,phone)values('$name','$email','$password','$phone')";
+    $data = mysqli_query($con,$query);
+    if($data){
+        //echo"Data inserted in Database";
+        header('location:index.php');
+    }else{
+        die(mysqli_error($con));
+    }
+}
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>CRUD</title>
+    <style>
+        h3{
+            text-align:center;
+        }
+    </style>
+  </head>
+  <body>
+    <h3>CRUD OPERATION</h3>
+    <div class="container">
+        <form method="post" >
+        <div class="mb-3">
+    <label class="form-label">Name</label>
+    <input type="text" name="name"class="form-control" id="name"placeholder="Enter your name"autocomplete="off" Required>
+   
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" name="email"class="form-control" id="email"placeholder="Enter your email"autocomplete="off"Required>
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" name="password"class="form-control" id="password"placeholder="Enter your password"autocomplete="off"Required>
+  
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Phone</label>
+    <input type="phone" name="phone"class="form-control" id="phone"placeholder="Enter your phone number"autocomplete="off"Required>
+   
+  </div>
+  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+</form>
+    </div>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+       
+  </body>
+</html>
